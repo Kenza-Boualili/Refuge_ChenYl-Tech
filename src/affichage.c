81f442845092f735bd0262d5020509777d918fb4
kenzaboualili <kenza.bll78@gmail.com>
2025-05-07 15:49:11 +0200

    Update affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include "affichage.h"
-#include "animal.h" 
-#define PINK    "\033[95m"
-#define YELLOW  "\033[93m"
+#include "animal.h"
+
+#define ROSE    "\033[95m"
+#define JAUNE   "\033[93m"
 #define CYAN    "\033[96m"
-#define RESET   "\033[0m"
+#define REINITIALISER  "\033[0m"
+
+// Affiche les informations complètes d'un animal donné
 void afficherResultat(const Animal *a, int age_calcule) {
     if (!a) return;
 
     printf("\n--- Résultat ---\n");
-    printf("ID         : %d\n", a->id);
-    printf("Nom        : %s\n", a->nom);
-    printf("Espèce     : %s\n", especeToStr(a->espece));
-    printf("Année Naiss: %d\n", a->annee_naissance);
-    printf("Poids      : %.2f kg\n", a->poids);
-    printf("Âge calculé: %d ans\n", age_calcule);
-    printf("Commentaire: %s\n", a->commentaire);
+    printf("ID          : %d\n", a->id);
+    printf("Nom         : %s\n", a->nom);
+    printf("Espèce      : %s\n", especeVersChaine(a->espece));
+    printf("Année Naiss : %d\n", a->annee_naissance);
+    printf("Poids       : %.2f kg\n", a->poids);
+    printf("Âge calculé : %d ans\n", age_calcule);
+    printf("Commentaire : %s\n", a->commentaire);
 }
 
+// Affiche le sous-menu pour choisir le type d’âge à afficher
 int menuTypeAge(void) {
     int choix = 0;
     printf("Afficher :\n");
@@ -30,19 +34,19 @@ int menuTypeAge(void) {
     return choix;
 }
 
-
+// Affiche le menu principal
 void afficherMenu() {
-    printf(PINK "╔══════════════════════════════════════╗\n" RESET);
-    printf(PINK "║ " CYAN "🏠  Bienvenue au Refuge ChenYl-Tech  " PINK "║\n" RESET);
-    printf(PINK "╚══════════════════════════════════════╝\n" RESET);
+    printf(ROSE "╔══════════════════════════════════════╗\n" REINITIALISER);
+    printf(ROSE "║ " CYAN "🏠  Bienvenue au Refuge ChenYl-Tech  " ROSE "║\n" REINITIALISER);
+    printf(ROSE "╚══════════════════════════════════════╝\n" REINITIALISER);
 
-    printf(YELLOW "1️⃣  Ajouter un animal        🐕 🐈\n" RESET);
-    printf(YELLOW "2️⃣  Rechercher des animaux   🔍🐾\n" RESET);
-    printf(YELLOW "3️⃣  Inventaire du refuge     📋🐾\n" RESET); 
-    printf(YELLOW "4️⃣  Nourriture quotidienne   🍖🧮\n" RESET);
-    printf(YELLOW "5️⃣  Faire adopter un animal  🏡❤️\n" RESET); 
-    printf(YELLOW "6️⃣  Quitter                  ❌👋\n" RESET);
+    printf(JAUNE "1️⃣  Ajouter un animal        🐕 🐈\n" REINITIALISER);
+    printf(JAUNE "2️⃣  Rechercher des animaux   🔍🐾\n" REINITIALISER);
+    printf(JAUNE "3️⃣  Inventaire du refuge     📋🐾\n" REINITIALISER); 
+    printf(JAUNE "4️⃣  Nourriture quotidienne   🍖🧮\n" REINITIALISER);
+    printf(JAUNE "5️⃣  Faire adopter un animal  🏡❤️\n" REINITIALISER); 
+    printf(JAUNE "6️⃣  Quitter                  ❌👋\n" REINITIALISER);
 
-    printf(CYAN "\n👉 Choix : " RESET);
+    printf(CYAN "\n👉 Choix : " REINITIALISER);
 }
 
