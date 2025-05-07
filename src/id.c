e14dfe4556960bce3b6a16e0934e7eb2bf8af551
kenzaboualili <kenza.bll78@gmail.com>
2025-05-07 15:50:59 +0200

    Update id.c

--- src/id.c
+++ src/id.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 
+// Génère un identifiant aléatoire basé sur l'heure et un décalage
 int genererID() {
     return (int)time(NULL) % 100000 + rand() % 100;
 }
