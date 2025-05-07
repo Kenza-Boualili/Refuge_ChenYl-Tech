5109b5a1f9cf27c4dbd7b153a39ed4a8e9ebef27
kenzaboualili <kenza.bll78@gmail.com>
2025-05-07 15:50:39 +0200

    Update comparer.c

--- src/comparer.c
+++ src/comparer.c
@@ -2,8 +2,8 @@
 
 int comparer(const char *s1, const char *s2) {
     int i = 0;
-    while(s1[i] && s2[i]) {
-        if((s1[i] | 32) != (s2[i] | 32)) return 0; // Insensible à la casse
+    while (s1[i] && s2[i]) {
+        if ((s1[i] | 32) != (s2[i] | 32)) return 0;
         i++;
     }
     return (s1[i] == '\0' && s2[i] == '\0');
