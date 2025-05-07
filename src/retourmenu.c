3cbfe15e241106c18745b6f7fd1b1acd19a5accd
kenzaboualili <kenza.bll78@gmail.com>
2025-05-07 15:54:17 +0200

    Update retourmenu.c

--- src/retourmenu.c
+++ src/retourmenu.c
@@ -2,26 +2,25 @@
 #include "retourmenu.h" 
 #include "utils.h"       
 
-#define YELLOW "\033[1;33m"
-#define RESET "\033[0m"
+#define JAUNE  "\033[1;33m"
+#define REINITIALISER "\033[0m"
 
-
+// Demande à l'utilisateur s’il souhaite revenir au menu
 int demanderRetourMenu() {
-    char buffer[10];
+    char tampon[10];
 
-    printf("\n" YELLOW "Appuyez sur 'm' + Entrée pour retourner au menu, ou juste Entrée pour continuer... " RESET);
+    printf("\n" JAUNE "Appuyez sur 'm' + Entrée pour retourner au menu, ou juste Entrée pour continuer... " REINITIALISER);
 
-    if (!fgets(buffer, sizeof(buffer), stdin)) {
+    if (!fgets(tampon, sizeof(tampon), stdin)) {
         return 0;
     }
 
-  
-    if ((buffer[0] == 'm' || buffer[0] == 'M') &&
-        (buffer[1] == '\n' || (buffer[1] == '\r' && buffer[2] == '\n') || buffer[1] == '\0') ) {
-         return 1;
+    if ((tampon[0] == 'm' || tampon[0] == 'M') &&
+        (tampon[1] == '\n' || (tampon[1] == '\r' && tampon[2] == '\n') || tampon[1] == '\0')) {
+        return 1;
     }
-
 
     return 0;
 }
+
 
