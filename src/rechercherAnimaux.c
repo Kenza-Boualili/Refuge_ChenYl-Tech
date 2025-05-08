46bb0b43ff925a4a33307cc4d135ebb27e062ce7
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 17:18:14 +0200

    Update rechercherAnimaux.c

--- src/rechercherAnimaux.c
+++ src/rechercherAnimaux.c
@@ -30,54 +30,6 @@ void copierTexte(char *destination, const char *source, int taille_max) {
     destination[i] = '\0';
 }
 
-int extraireChamps(char *ligne, Animal *animal, char *especeTemp, char *commentaireTemp) {
-    int champ = 0, index = 0, i = 0;
-    char morceau[256];
-
-    while (ligne[i]) {
-        if (ligne[i] == ';' || ligne[i] == '\n' || ligne[i] == '\0') {
-            morceau[index] = '\0';
-
-            switch (champ) {
-                case 0:
-                    animal->id = atoi(morceau);
-                    break;
-                case 1:
-                    copierTexte(animal->nom, morceau, TAILLE_NOM);
-                    break;
-                case 2:
-                    copierTexte(especeTemp, morceau, 50);
-                    break;
-                case 3:
-                    animal->annee_naissance = atoi(morceau);
-                    break;
-                case 4:
-                    animal->poids = atof(morceau);
-                    break;
-                case 5:
-                    copierTexte(commentaireTemp, morceau, TAILLE_COMM);
-                    break;
-                default:
-                    break;
-            }
-
-            champ++;
-            index = 0;
-
-            if (ligne[i] == '\n' || ligne[i] == '\0') {
-                break;
-            }
-        } else {
-            if (index < sizeof(morceau) - 1) {
-                morceau[index++] = ligne[i];
-            }
-        }
-        i++;
-    }
-
-    return champ >= 5;
-}
-
 // Calcule l’âge à partir de l’année de naissance
 static int calculerAge(int annee_naissance) {
     if (annee_naissance <= 0){
