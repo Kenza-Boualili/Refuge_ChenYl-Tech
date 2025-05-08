a88d09152dbec544ce3bf2d6ffe2be89a441dbaf
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 20:19:56 +0200

    Update animal.h

--- include/animal.h
+++ include/animal.h
@@ -1,8 +1,56 @@
-// Si le symbole 'COMPARER_H' n'a pas encore été défini:
-#ifndef COMPARER_H
-//on le définit maintenant.
-#define COMPARER_H
 
-int comparer(const char *s1, const char *s2);
-int comparerAnimauxParEspece(const void *a, const void *b);
-#endif 
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#define MAX_ANIMAUX 100
+#define TAILLE_COMM 256
+#define TAILLE_NOM 50
+
+
+
+// Définition de l'énumération des espèces
+
+typedef enum Espece {
+CHIEN,
+ CHAT,
+HAMSTER,
+ AUTRUCHE,
+ INCONNU
+
+} Espece;
+// Structure représentant un animal
+typedef struct Animal {
+
+    int id;
+
+    char nom[TAILLE_NOM];  // Utilisation de TAILLE_NOM
+
+    Espece espece;
+
+    int annee_naissance;
+
+    float poids;
+
+    char commentaire[TAILLE_COMM];
+
+} Animal;
+// Déclarations des fonctions
+
+Espece chaineVersEspece(const char *str);
+
+const char* especeVersChaine(Espece e);
+
+void enleverSautLigne(char *chaine, int taille_max);
+
+Espece choisirEspece();
+#endif // ANIMAL_H
+
+
+
+
+
+
+
+
+
+
+
