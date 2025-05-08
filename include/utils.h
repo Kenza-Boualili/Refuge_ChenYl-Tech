1037da9ac4464954ccd3595567e15cd1369d6fd0
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 19:20:56 +0200

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -2,11 +2,16 @@
 #define UTILS_H
 
 #include <stddef.h>
+#include "animal.h"
+
 
 void nettoyerTampon();
 void enleverSautLigne(char *chaine, size_t taille_max);
 size_t longueurChaine(const char *chaine);
+void nettoyerLigne(char *ligne);
 int extraireChamps(char *ligne, Animal *animal, char *especeTemp, char *commentaireTemp);
+void copierTexte(char *destination, const char *source, int max_size);
+
 
 
 #endif
