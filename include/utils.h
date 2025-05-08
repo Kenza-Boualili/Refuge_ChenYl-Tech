dfe0393cf1f03608de456320c9b0d06ca97637e7
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 22:05:58 +0200

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -10,7 +10,7 @@ void enleverSautLigne(char *chaine, int taille_max);
 size_t longueurChaine(const char *chaine);
 void nettoyerLigne(char *ligne);
 int extraireChamps(char *ligne, Animal *animal, char *especeTemp, char *commentaireTemp);
-void copierTexte(char *destination, const char *source, int max_size);
+void copierTexte(char *destination, const char *source, size_t max_size);
 
 
 
