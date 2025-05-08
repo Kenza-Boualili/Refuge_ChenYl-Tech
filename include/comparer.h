97521471c14b7528be41942026de0b473230c139
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 20:10:12 +0200

    Update comparer.h

--- include/comparer.h
+++ include/comparer.h
@@ -1,14 +1,11 @@
-// Si le symbole 'COMPARER_H' n'a pas encore été défini:
 #ifndef COMPARER_H
-//on le définit maintenant.
 #define COMPARER_H
-
-// 'int' indique que la fonction retourne un nombre entier.
-//  pour une fonction de comparaison, la valeur retournée indique le résultat de la comparaison :
-//     - 0 si les deux chaînes sont égales.
-//     - une valeur négative si la première chaîne (s1) est "avant" la seconde (s2) dans l'ordre alphabétique.
-//     - une valeur positive si la première chaîne (s1) est "après" la seconde (s2) dans l'ordre alphabétique.
-//   - 'const char *s1' : Un pointeur vers une chaîne de caractères.
-//   - 'const char *s2' : Un pointeur vers une seconde chaîne de caractères,
+#include "animal.h"
+// Fonction pour comparer deux chaînes de caractères sans tenir compte de la casse
 int comparer(const char *s1, const char *s2);
+
+// Fonction pour comparer deux animaux par espèce
+int comparerAnimauxParEspece(const void* a1, const void* a2);
+
+
 #endif // COMPARER_H
