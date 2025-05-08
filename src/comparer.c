52f976b7e2f62b038996954ee7d83a87bdf258a8
kenzaboualili <kenza.bll78@gmail.com>
2025-05-08 20:05:49 +0200

    Update comparer.c

--- src/comparer.c
+++ src/comparer.c
@@ -1,5 +1,15 @@
 #include "../include/comparer.h"
 
+
+int comparerAnimauxParEspece(const void* a, const void* b) {
+    const Animal* animal1 = (const Animal*)a;
+    const Animal* animal2 = (const Animal*)b;
+
+    return animal1->espece - animal2->espece;
+}
+
+
+
 int comparer(const char *s1, const char *s2) {
     int i = 0;
     while (s1[i] && s2[i]) {
