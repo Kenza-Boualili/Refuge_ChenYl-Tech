ecc1b608243111dd49146ecad7e691c340b87327
kenzaboualili <kenza.bll78@gmail.com>
2025-05-13 23:46:33 +0200

    Update affichage.h

--- include/affichage.h
+++ include/affichage.h
@@ -9,7 +9,4 @@ void afficherMenu(void);
 // Affiche les détails d'un animal et son âge (réel ou humain)
 void afficherResultat(const Animal *a, int age_calcule);
 
-// Menu pour choisir le type d’âge à afficher (réel ou humain)
-int menuTypeAge(void);
-
 #endif
